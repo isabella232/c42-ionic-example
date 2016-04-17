f2cddb2ec65d26bfb81ed2ab049c9aa02e8b0596
Jasper Hartong <Jasper@calendar42.com>
2016-04-17 21:31:55 +0200

    simplify setting scope

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -54,20 +54,9 @@ angular.module('c42-ionic.controllers', [])
 
 
   // BEGIN setting event data
-  var setScope = function () {
-    c42Api.getEventById($stateParams.eventId, function (event) {
-      $scope.event = event;
-    });
-  };
-  // @edmon: I would like to have this function to work when I visit the page directly AND when from the app
-  // Now I need to do this workaround
-  try {
-    setScope();
-  } catch(e) {
-    c42Api.onReady(function(){
-      setScope();
-    });
-  }
+  c42Api.getEventById($stateParams.eventId, function (event) {
+    $scope.event = event;
+  });
   // END setting event data
 }])
 
