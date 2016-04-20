d031092433def852c171326fc42516695b5983ca
Edmon <comlaterra@gmail.com>
2016-04-20 15:32:44 +0200

    Add non-elegant way to filter the events from the map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('c42-ionic.controllers', [])
 
-.controller('HomeCtrl', ['$scope', 'config', 'local_settings', 'c42Api','calendarFilter', '$window', function($scope,config, local_settings, c42Api, calendarFilter, $window) {
+.controller('HomeCtrl', ['$scope', 'config', 'local_settings', 'c42Api','calendarFilter', '$window', 'uiGmapGoogleMapApi', function($scope,config, local_settings, c42Api, calendarFilter, $window, uiGmapGoogleMapApi) {
   var options = {
     // As we want to show quite some markers on the map, this is now set to 100.
     // If the markers and events in the list are pulled of different API calls this could be set to 10
@@ -20,6 +20,49 @@ angular.module('c42-ionic.controllers', [])
   };
 
   $scope.mapConfig = config.initialConfig.mapDefaults;
+
+  $scope.mapConfig.events = {
+
+    'idle':function(){
+      c42Api.getEvents(options, function(events){
+          $scope.$apply(function () {
+              $scope.events = $scope.events ? $scope.events.concat(events) : events;
+          });
+          options.offset += $scope.events.length;
+      });
+    },
+    'bounds_changed': function(){
+      // getting the polyline to be encoded and sent to get the events included in it
+      var mapBounds = this.map.getBounds();
+      var bounds = [
+        // Nordth - West
+        [
+          this.map.getBounds().getNorthEast().lat(),
+          this.map.getBounds().getSouthWest().lng()
+        ],
+        // Nordth - East
+        [
+          this.map.getBounds().getNorthEast().lat(),
+          this.map.getBounds().getNorthEast().lng()
+        ],
+        // South - East
+        [
+          this.map.getBounds().getSouthWest().lat(),
+          this.map.getBounds().getNorthEast().lng()
+        ],// South - West
+        [
+          this.map.getBounds().getSouthWest().lat(),
+          this.map.getBounds().getSouthWest().lng()
+        ],
+        // Nordth - West (closing the square)
+        [
+          this.map.getBounds().getNorthEast().lat(),
+          this.map.getBounds().getSouthWest().lng()
+        ]
+      ];
+      options.geo_polygons = "["+polyline.encode(bounds)+"]";
+    }
+  };
   // @TODO: Add this to the resolve in the way that is filtered before of being rendered
   // @TODO: Get it form the BE
   $scope.$on('$ionicView.enter', function() {
@@ -46,14 +89,20 @@ angular.module('c42-ionic.controllers', [])
   };
 
   $scope.loadMore = function () {
-    c42Api.getEvents(options, function(events){
-        $scope.$apply(function () {
-            $scope.events = $scope.events ? $scope.events.concat(events) : events;
-        });
-        options.offset += $scope.events.length;
-        // Notify that the infinite loading is complete
-        $scope.$broadcast('scroll.infiniteScrollComplete');
-    });
+    // We don't want to load with load More at the beginning. Only due user scroll
+    if($scope.events){
+      c42Api.getEvents(options, function(events){
+          $scope.$apply(function () {
+              $scope.events = $scope.events ? $scope.events.concat(events) : events;
+          });
+          options.offset += $scope.events.length;
+          // Notify that the infinite loading is complete
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    }else{
+      // Because this method will be called even if there is none, we stop it notifying
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    }
   };
 
   // As the infinite-scroll directive is already checking whether to load more, it will automaticaly load on entering the view
